752927986556496a4e2a0fc2f89b5faa36df33e7
risethesummer <honhatlinhmail@gmail.com>
2023-03-28 23:57:06 +0700

    done sign up user

--- src/authenticationService/src/main/java/com/group_1/authenticationService/service/AccountServiceImpl.java
+++ src/authenticationService/src/main/java/com/group_1/authenticationService/service/AccountServiceImpl.java
@@ -2,10 +2,14 @@ package com.group_1.authenticationService.service;
 
 import com.group_1.authenticationService.dto.CreateAccountRequestDto;
 import com.group_1.authenticationService.dto.CreateAccountResponseDto;
+import com.group_1.sharedAws.model.UserInfo;
+import com.group_1.sharedAws.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.*;
+
+import java.time.LocalDateTime;
 
 /**
  * service
@@ -20,22 +24,37 @@ public class AccountServiceImpl implements AccountService {
     private String cognitoClientId;
 
     private final CognitoIdentityProviderClient cognitoIdentityProviderClient;
+    private final UserRepository userRepository;
 
-    public AccountServiceImpl(CognitoIdentityProviderClient cognitoIdentityProviderClient) {
+    public AccountServiceImpl(CognitoIdentityProviderClient cognitoIdentityProviderClient,
+                              UserRepository userRepository) {
         this.cognitoIdentityProviderClient = cognitoIdentityProviderClient;
+        this.userRepository = userRepository;
     }
 
     public CreateAccountResponseDto createAccount(CreateAccountRequestDto createAccountRequestDto)
     {
+        String email = createAccountRequestDto.email();
         AttributeType userAttrs = AttributeType.builder()
                 .name("email")
-                .value(createAccountRequestDto.email())
+                .value(email)
                 .build();
+        LocalDateTime now = LocalDateTime.now();
         SignUpResponse response = cognitoIdentityProviderClient.signUp(builder -> builder
                     .clientId(cognitoClientId)
                     .username(createAccountRequestDto.email())
                     .userAttributes(userAttrs)
                     .password(createAccountRequestDto.password()));
-        return new CreateAccountResponseDto(response.userSub(), createAccountRequestDto.email());
+        userRepository.saveRecord(UserInfo
+                .builder()
+                .userId(response.userSub())
+                .email(email)
+                .storedFileCount(0L)
+                .usedSpace(0L)
+                .maxSpace(0L)
+                .storagePackId("default")
+                .purchasedPackDate(now.toString())
+                .build());
+        return new CreateAccountResponseDto(response.userSub(), email);
     }
 }
